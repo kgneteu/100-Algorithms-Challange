2e5fbaeca6e7c94d428729f331921ef6b2a473cb
kgneteu <77528950+kgneteu@users.noreply.github.com>
2022-02-12 22:55:41 +0100

    alphabeticShift

---
+++ ts/src/alphabeticShift.ts
@@ -0,0 +1,35 @@
+// Given a string, replace each its character by the next one in the English alphabet (z would be replaced by a).
+//
+// Example
+//
+// For inputString = "crazy", the output should be alphabeticShift(inputString) = "dsbaz".
+//
+//     Hints
+//
+// split()
+// indexOf()
+// join()
+// Input/Output
+//
+//     [time limit] 4000ms (js)
+//     [input] string inputString
+// Non-empty string consisting of lowercase English characters.
+//
+//     Guaranteed constraints:
+//
+//     1 ≤ inputString.length ≤ 10.
+//
+//     [output] string
+// The result string after replacing all of its characters.
+
+export function alphabeticShift(s: string): string {
+    let out: string = '';
+    for (let c of s) {
+        if (c === 'z') {
+            out += 'a';
+        } else {
+            out += String.fromCharCode((c.charCodeAt(0) + 1));
+        }
+    }
+    return out;
+}
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -3,6 +3,7 @@ import {addTwoDigits} from "./addTwoDigits";
 import {add, add2} from "./addNumbers";
 import {adjacentElementsProduct} from "./adjacentElementsProduct";
 import {allLongestStrings} from "./allLongestStrings";
+import {almostIncreasingSequence} from "./almostIncreasingSequence";
 
 console.log('-------------------------------------------------------\n')
 console.log('addBorder\n',addBorder(["abc","bcd"]).join('\n'));
@@ -11,4 +12,6 @@ console.log('addTwoDigits\n',addTwoDigits(29));
 console.log('addNumbers\n',add(1,2));
 console.log(add2(1,2, 3));
 console.log('adjacentElementsProduct\n',adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log('almostIncreasingSequence\n',almostIncreasingSequence([1, 3, 2, 1]));
+
 console.log('-------------------------------------------------------\n')
