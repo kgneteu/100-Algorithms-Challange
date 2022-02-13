12c3228f4a272c98b9a9256eb31d06af639d4617
kgneteu <77528950+kgneteu@users.noreply.github.com>
2022-02-13 03:59:03 +0100

    add_numbers

---
+++ python/tests/test_add_numbers.py
@@ -0,0 +1,19 @@
+import unittest
+
+from src.add_numbers import add, add2
+
+
+class TestAddNumbers(unittest.TestCase):
+    def test_simple_add(self):
+        self.assertEqual(add(1, 2), 3)
+
+    def test_var_add(self):
+        self.assertEqual(add2(1, 2, 3, 4), 10)
+
+    def test_var_add_array(self):
+        list_of_integers = [1, 2, 3]
+        self.assertEqual(add2(*list_of_integers), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
