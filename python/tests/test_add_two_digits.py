e2fa7a20474ac1033b9516f0d6734cb0d4a9d19d
kgneteu <77528950+kgneteu@users.noreply.github.com>
2022-02-13 04:32:13 +0100

    add_two_digits

---
+++ python/tests/test_add_two_digits.py
@@ -0,0 +1,12 @@
+import unittest
+
+from src.add_two_digits import add_two_digits
+
+
+class TestAddTwoDigits(unittest.TestCase):
+    def test_add_two_digits(self):
+        self.assertEqual(add_two_digits(11), 2)
+        self.assertEqual(add_two_digits(341), 8)
+
+if __name__ == '__main__':
+    unittest.main()
