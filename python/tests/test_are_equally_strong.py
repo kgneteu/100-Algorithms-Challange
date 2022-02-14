080e368f217ecf9b4ba2f957c7b1e9fd6e0863d4
kgneteu <77528950+kgneteu@users.noreply.github.com>
2022-02-14 22:21:24 +0100

    python refactor

--- python/tests/test_are_equally_strong.py
+++ python/tests/test_are_equally_strong.py
@@ -3,10 +3,16 @@ from src.are_equally_strong import are_equally_strong
 
 
 class TestAreEquallyStrong(unittest.TestCase):
-    def test_are_equally_strong(self):
+    def test_are_equally_strong1(self):
         self.assertTrue(are_equally_strong(10, 15, 15, 10))
+
+    def test_are_equally_strong2(self):
         self.assertTrue(are_equally_strong(15, 10, 15, 10))
+
+    def test_are_equally_strong3(self):
         self.assertFalse(are_equally_strong(15, 10, 15, 9))
+
+    def test_are_equally_strong4(self):
         self.assertFalse(are_equally_strong(12, 10, 8, 14))
 
 
