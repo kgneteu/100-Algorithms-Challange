aeb5d6be7af16256fc658f061dbd37c42347c7b6
kgneteu <77528950+kgneteu@users.noreply.github.com>
2022-02-15 17:08:56 +0100

    ts A finished

--- ts/src/avoidObstacles.ts
+++ ts/src/avoidObstacles.ts
@@ -1,5 +1,43 @@
-export function avoidObstacles(inputArray: number[]): number {
+// You are given an array of integers representing coordinates
+// of obstacles situated on a straight line.
+//
+//     Assume that you are jumping from the point with coordinate 0 to the right.
+//     You are allowed only to make jumps of the same length represented by some integer.
+//
+//     Find the minimal length of the jump enough to avoid all the obstacles.
+//
+//     Example
+//
+// For inputArray = [5, 3, 6, 7, 9], the output should be avoidObstacles(inputArray) = 4.
+//
+// Hints
+//
+// sort()
+// every()
+// Input/Output
+//
+//     [time limit] 4000ms (js)
+//     [input] array.integer inputArray
+// Non-empty array of positive integers.
+//
+//     Guaranteed constraints:
+//
+//     2 ≤ inputArray.length ≤ 10,
+//
+//     1 ≤ inputArray[i] ≤ 40.
+//
+//     [output] integer
+// The desired length.
+//
 
+export function avoidObstacles(inputArray: number[]): number {
+    const max = Math.max(...inputArray);
+    let minLength = max + 1;
+    for (let i = 2; i < max - 1; i++) {
+        if (inputArray.every(v => v % i !== 0)) {
+            return i;
+        }
+    }
+    return minLength;
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
--- ts/tests/arrayReplace.spec.ts
+++ ts/tests/arrayReplace.spec.ts
@@ -2,15 +2,11 @@ import { arrayReplace } from '../src/arrayReplace';
 
 describe(arrayReplace.name, () => {
     it('Test 1', () => {
-       
+
         const data = [3, 5, 2, 4, 5];
-        const elementToReplace = 1;
-        const substitutionElement = 3;
-
-   
+        const elementToReplace = 5;
+        const substitutionElement = 8;
         const response = arrayReplace(data, elementToReplace, substitutionElement);
-
-
-        expect(response).toEqual([3, 2, 3]);
+        expect(response).toEqual([3, 8, 2, 4, 8]);
     });
 });
