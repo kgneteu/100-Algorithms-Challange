323f2fd52f2177b7b243f0ef58e37326d886b830
kgneteu <77528950+kgneteu@users.noreply.github.com>
2022-02-28 07:05:07 +0100

    lateRide

--- ts/src/lateRide.ts
+++ ts/src/lateRide.ts
@@ -1,15 +1,25 @@
-// One night you go for a ride on your motorcycle. At 00:00 you start your engine, and the built-in timer automatically begins counting the length of your ride, in minutes. Off you go to explore the neighborhood.
+// One night you go for a ride on your motorcycle.
+// At 00:00 you start your engine, and the built-in timer automatically begins
+// counting the length of your ride, in minutes.
+// Off you go to explore the neighborhood.
 //
-//     When you finally decide to head back, you realize there's a chance the bridges on your route home are up, leaving you stranded! Unfortunately, you don't have your watch on you and don't know what time it is. All you know thanks to the bike's timer is that n minutes have passed since 00:00.
+// When you finally decide to head back, you realize there's a chance the
+// bridges on your route home are up,
+// leaving you stranded!
+// Unfortunately, you don't have your watch on you and don't know what time it is.
+// All you know thanks to the bike's timer is that n minutes have passed since 00:00.
 //
-//     Using the bike's timer, calculate the current time. Return an answer as the sum of digits that the digital timer in the format hh:mm would show.
+// Using the bike's timer, calculate the current time.
+// Return an answer as the sum of digits that the digital timer in the format hh:mm
+// would show.
 //
 // Example
 //
 // For n = 240, the output should be lateRide(n) = 4.
-// Since 240 minutes have passed, the current time is 04:00. The digits sum up to 0 + 4 + 0 + 0 = 4, which is the answer.
+// Since 240 minutes have passed, the current time is 04:00.
+// The digits sum up to 0 + 4 + 0 + 0 = 4, which is the answer.
 //
-//     For n = 808, the output should be lateRide(n) = 14.
+// For n = 808, the output should be lateRide(n) = 14.
 // 808 minutes mean that it's 13:28 now, so the answer should be 1 + 3 + 2 + 8 = 14.
 //
 // Hints
@@ -22,8 +32,8 @@
 // map()
 // Input/Output
 //
-//     [execution time limit] 4 seconds (js)
-//     [input] integer n
+// [execution time limit] 4 seconds (js)
+// [input] integer n
 // The duration of your ride, in minutes. It is guaranteed that you've been riding for less than a day (24 hours).
 //
 // Guaranteed constraints:
@@ -32,9 +42,16 @@
 //
 //     [output] integer
 // The sum of the digits the digital timer would show.
-export function lateRide(n: number): number {
 
+export function lateRide(n: number): number {
+    // const [h, m] = [(n - n % 60) / 60, n % 60]
+    // return (h - h % 10) / 10 + h % 10 + (m - m % 10) / 10 + m % 10;
+
+    //Alt
+    let [h, m] = [n / 60 | 0, n % 60]
+    return (h / 10 | 0) + h % 10 + (m / 10 | 0) + m % 10;
 }
+
 
 // console.log(lateRide(240));
 // console.log(lateRide(808));
--- ts/tests/lateRide.spec.ts
+++ ts/tests/lateRide.spec.ts
@@ -6,10 +6,24 @@ describe(lateRide.name, () => {
         const response = lateRide(data);
         expect(response).toBe(4);
     });
-
     it('Test 2', () => {
         const data = 808;
         const response = lateRide(data);
         expect(response).toBe(14);
+    });
+    it('Test 3', () => {
+        const data = 1439;
+        const response = lateRide(data);
+        expect(response).toBe(19);
+    });
+    it('Test 4', () => {
+        const data = 0;
+        const response = lateRide(data);
+        expect(response).toBe(0);
+    });
+    it('Test 5', () => {
+        const data = 8;
+        const response = lateRide(data);
+        expect(response).toBe(8);
     });
 });
