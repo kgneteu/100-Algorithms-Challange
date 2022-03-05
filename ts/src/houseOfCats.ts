81ef3979348389e7a94842c3ef7626f78762ee1c
kgneteu <77528950+kgneteu@users.noreply.github.com>
2022-03-05 12:28:38 +0100

    houseOfCats

--- ts/src/houseOfCats.ts
+++ ts/src/houseOfCats.ts
@@ -1,5 +1,6 @@
 // There are some people and cats in a house. You are given the number of legs
-// they have all together. Your task is to return an array containing every possible number
+// they have all together. Your task is to return an array containing
+// every possible number
 // of people that could be in the house  sorted in ascending order.
 // It's guaranteed that each person has 2 legs and each cat has 4 legs.
 //
